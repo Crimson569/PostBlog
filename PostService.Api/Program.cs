8133eb16e01769ab70f00f65688fdd8b29477c69
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-09-05 14:01:33 +0300

    PostServiceInitialization: Инициализация Api слоя

---
+++ PostService.Api/Program.cs
@@ -0,0 +1,28 @@
+using PostService.Application.Extensions;
+using PostService.Infrastructure.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureApplicationServices();
+builder.Services.ConfigureInfrastructureServices(builder.Configuration);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
